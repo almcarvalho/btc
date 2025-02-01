9c9640efa1420ff9daff3ec7d7ad7dccb4b5baf7
Lucas Carvalho <antoniolucas@live.com>
2025-02-01 10:23:53 -0300

    here we go

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(express.json()); 
 
-app.get('/hello', (req, res) => {
-    res.send('Hello, World!');
+var machine01 = 0;
+
+app.get('/check-payment', (req, res) => {
+    var temp = machine01;
+    machine01 = 0;
+    res.send(""+temp);
 });
 
 // Webhook route
 app.post('/webhook', (req, res) => {
     console.log('Webhook received:', req.body); // Now req.body will be parsed correctly
+    machine01 = 100;
     res.status(200).send('Webhook received!');
 });
 
