9786b741cc5ad4b29de660c50e6af76bcc25d230
Lucas Carvalho <antoniolucas@live.com>
2025-02-08 20:24:32 -0300

    formatando pulsos

--- server.js
+++ server.js
@@ -6,11 +6,36 @@ const PORT = process.env.PORT || 3000;
 
 // Svix Webhook Secret (Ensure this is stored securely, e.g., via environment variables)
 const SVIX_SECRET = process.env.SVIX_SECRET;
+const SATOSHI_BRL = 178;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 var machine01 = 0;
+
+//1 real ou 1 BRL = 178 satoshis
+function converterSatoshis(valor) {
+    var valorAux = 0;
+    var ticket = 0;
+    ticket = SATOSHI_BRL; 
+
+    if (valor >= ticket) {
+        valorAux = valor;
+        valor = 0;
+        //creditos
+        var creditos = valorAux / ticket;
+        creditos = Math.floor(creditos);
+        var pulsosFormatados = ("0000" + creditos).slice(-4);
+        return pulsosFormatados;
+    } else {
+        return "0000";
+    }
+}
+
+// teste da nossa funcao auxiliar
+app.post('/check-payment-test', (req, res) => {
+    res.send("" + converterSatoshis(req.body.valor));
+});
 
 // GET route to check payment status
 app.get('/check-payment', (req, res) => {
